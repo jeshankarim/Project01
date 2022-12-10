be4a98ef091a6bfa3eb84a2b96b32edb77745c83
Jeshan <jeshankarim7666@gmai.com>
2022-12-10 21:18:30 +0530

    first commit

---
+++ cake.py
@@ -0,0 +1,16 @@
+cakeangle=int(input("Enter the Angle of the Cake: "))
+N=int(input("Enter the no of pieces of the Cake: "))
+if(cakeangle%N==0):
+    print("YES the cake will cut in equal pieces of size",N)
+else:
+    print("NO the cake will not cut in equal pieces of size ",N)
+if(N>cakeangle):#only when N is greater the cake angle cake can't be cut into pieces
+    print("NO the cake will not cut in any pieces of size ",N)
+else:
+    print("YES the cake will cut in any pieces of size",N)
+#start substracting the cake 
+if (cakeangle<0):
+    print("NO the cake will not cut into pieces such that no two of them are equal",N)
+        
+else:
+    print("YES the cake will cut into pieces such that no two of them are equal",N)   
